0b9ebbaa6b2a54a3afdc5baa2f417cf8323cd5e1
Víctor Felipe Godoy Hernández <victor.godoy@infovida.cl>
2017-02-03 17:49:18 -0300

    endpoint returns json

--- index.ts
+++ index.ts
@@ -2,10 +2,16 @@
  * Created by vigohe on 25-01-17.
  */
 import express = require('express');
+import bodyParser = require("body-parser");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser(bodyParser.json()));
+
+app.get('/', (req, res) => res.json({
+    msg: "Hello World"
+}));
 
 const server = app.listen(3000, () =>
     console.log(`Server running at http://localhost:' ${server.address().port}`)
